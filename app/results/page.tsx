d3e1726e45c74a4b39c9527c3581c137debac9e4
onairx <vnadipudi@outlook.com>
2025-05-25 19:37:37 +0530

    minor bug fixes

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Results from './web/page'
 
 export default function Page() {
     return (
-        <>
+        <Suspense
+            fallback={
+                <div className="w-full h-full flex justify-start items-center py-20 px-5 text-5xl font-bold text-[#0a0a0a]">
+                    Loading..
+                </div>
+            }
+        >
             <Results />
-        </>
+        </Suspense>
     )
 }
