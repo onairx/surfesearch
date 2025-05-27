2f3fae51b737d82ae774a8c5496a2f45454c013a
onairx <vnadipudi@outlook.com>
2025-05-27 12:03:04 +0530

    minor ui change

--- app/crave/page.tsx
+++ app/crave/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Crave() {
     return (
-        <section className="w-[100vw] h-[100vh] p-5 flex flex-col items-center justify-center text-center gap-5">
+        <section className="w-[100vw] h-[100vh] p-5 flex flex-col items-center justify-center text-center gap-5 bg-[#f4f1de] text-[#0a0a0a]">
             <div className="w-full h-[30vh] flex justify-center items-center">
                 <Image src={waitingCat} alt="waiting cat" width={300} height={300} />
             </div>
